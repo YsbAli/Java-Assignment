855a75f5eeb2902f89d2f04a76f9bdaed8fed1fe
Yousub Ali <ysbofficial122@gmail.com>
2023-05-14 23:56:50 +0530

    InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-public class Input {
+public class InputOutput {
     public static void main(String[] args)
         throws IOException
     {
         BufferedReader reader = new BufferedReader(
             new InputStreamReader(System.in));
  
-        String name = reader.readLine();
+        String Input = reader.readLine();
  
-        System.out.println(name);
+        System.out.println(Input);
     }
 }
