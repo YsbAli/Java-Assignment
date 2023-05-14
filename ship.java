e625f5d38a2faebe8d1cbc7d7bfadda55ba49f77
Yousub Ali <ysbofficial122@gmail.com>
2023-05-14 23:23:47 +0530

    Nothing Here

--- ship.java
+++ ship.java
@@ -1,19 +1,1 @@
-import java.util.Scanner;
 
-public class ship{
-    int productId;
-    String Pname;
-}
-
-class customer extends ship{
-    String name;
-    customer(String n,int p,String pn){
-        name=n;
-        productId=p;
-        Pname=pn;
-    }
-    public static void main(String[] args){
-        customer s=new customer("Ram",25,"pen");
-        System.out.println("customer name:"+s.name+" product id:"+s.productId+" product name:"+s.Pname);
-    }
-}
