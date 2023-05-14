0a84aff7381a2a78574793aadc48b9b7a3851b76
Yousub Ali <ysbofficial122@gmail.com>
2023-05-15 00:02:20 +0530

    Delete packtest.java

--- packtest.java
+++
@@ -1,9 +0,0 @@
-import pack1.pack;
-
-public class packtest {
-    public static void main(String[] args){
-        System.out.println("this inside test class");
-        pack p=new pack();
-        p.test();
-    }
-}
